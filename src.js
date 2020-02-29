4111ddd86e2abd33687bbf913d990d82a4e556d2
Meena Erian <meena5.erian@gmail.com>
2020-02-29 23:47:57 +0200

    Update src.js

--- src.js
+++ src.js
@@ -7,7 +7,7 @@ window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.ms
 
 db = {};
 
-var request = window.indexedDB.open("database1");
+var request = window.indexedDB.open("database1", 3);
 
 request.onerror = function(event) {
     console.log("Error opening database:");
@@ -17,6 +17,34 @@ request.onerror = function(event) {
 request.onsuccess = function(event) {
     db = request.result;
 };
+
+request.onupgradeneeded = function(event) {
+  alert("upgrading database");
+  db = event.target.result;
+
+  db.onerror = function(event) {
+    console.log("Error opening database:");
+    console.log(event);
+  };
+
+  // Create an objectStore for this database using
+  // IDBDatabase.createObjectStore
+
+  var objectStore = db.createObjectStore("toDoList", { keyPath: "taskTitle" });
+
+  // define what data items the objectStore will contain
+
+  objectStore.createIndex("hours", "hours", { unique: false });
+  objectStore.createIndex("minutes", "minutes", { unique: false });
+  objectStore.createIndex("day", "day", { unique: false });
+  objectStore.createIndex("month", "month", { unique: false });
+  objectStore.createIndex("year", "year", { unique: false });
+
+  objectStore.createIndex("notified", "notified", { unique: false });
+
+  alert("objectStore created");
+};
+
 
 function insert(itm) {
    var request = db.transaction(["todos"], "readwrite").objectStore("todo")
